99c1045d5878a7408d24b1f8d6342f74a98678f4
Pedro Xavier <arclimatizadot@gmail.com>
2026-05-23 15:10:56 -0300

    Arrays, push, sort e indexOf
    Exercício da aula #21 de JavaScript - Curso em Vídeo

--- ambiente.js
+++ ambiente.js
@@ -1,5 +1,15 @@
-console.log('Hello...')
-for(var c = 1;c <= 3;c++) {
-    console.log(c)
+let num = [5, 4, 8]
+num.push(1)
+num.sort()
+console.log(`${num}`)
+console.log(`O vetor tem ${num.length} posições.`)
+console.log(`O primeiro valor da primeira posição é ${num[0]}`)
+
+let res = Number(prompt('Qual o número que você gostaria de saber a posição?'))
+let pos = num.indexOf(res)
+if (pos == -1) {
+    console.log(`O número (${res})não foi encontrado`)
 }
-console.log('World!')
+else {
+    console.log(`O valor ${res} está na posição ${pos}`)
+}
